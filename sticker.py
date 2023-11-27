420760252505d7a029c60ed0a584721c785fe80b
Lucas Tempass <lucas.tempass@hotmail.com>
2023-11-27 19:56:03 -0300

    Corrige overflow dos stickers

--- sticker.py
+++ sticker.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 class Sticker:
     def __init__(self, sticker_path):
         self.sticker = cv2.imread(sticker_path, -1)
@@ -15,20 +16,31 @@ class Sticker:
             print("Erro: As coordenadas x e y devem ser números inteiros.")
             return image
 
-        if 0 <= y < image.shape[0] and 0 <= x < image.shape[1]:
-            image_with_sticker = image.copy()
-            sticker_height, sticker_width, _ = self.sticker.shape
+        image_with_sticker = image.copy()
 
-            for i in range(sticker_height):
-                for j in range(sticker_width):
-                    alpha = self.sticker[i, j][3] / 255.0
-                    if alpha > 0:
-                        for c in range(3):
-                            image_with_sticker[y + i, x + j, c] = (1 - alpha) * image[y + i, x + j, c] + alpha * self.sticker[i, j, c]
+        sticker_height, sticker_width, _ = self.sticker.shape
 
-            return image_with_sticker
-        else:
-            print("Erro: Coordenadas fora dos limites da imagem.")
-            return image
+        for sticker_y in range(sticker_height):
+            for sticker_x in range(sticker_width):
+                alpha = self.sticker[sticker_y, sticker_x][3] / 255.0
 
-    
+                if alpha <= 0:
+                    continue
+
+                for c in range(3):
+                    # Mapeia as coordenadas do sticker para as coordenadas da imagem
+                    image_y = y + sticker_y
+                    image_x = x + sticker_x
+
+                    # Verifica se as coordenadas estão dentro dos limites da imagem
+                    if image_y >= image.shape[0] or image_x >= image.shape[1]:
+                        continue
+
+                    # Aplica a transparência
+                    sticker_channel = self.sticker[sticker_y, sticker_x, c] * alpha
+                    image_channel = (1 - alpha) * image[image_y, image_x, c]
+
+                    # Combina os canais da imagem e do sticker
+                    image_with_sticker[image_y, image_x, c] = image_channel + sticker_channel
+
+        return image_with_sticker
